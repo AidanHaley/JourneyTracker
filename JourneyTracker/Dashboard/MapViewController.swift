08e6d67be114493818528755ef28239f31fc52af
Aidan Haley <Aidan.h95@outlook.com>
2019-11-14 21:32:24 +0000

    Added map view and shows user location

--- JourneyTracker/Dashboard/MapViewController.swift
+++ JourneyTracker/Dashboard/MapViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    //Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Vars
+    fileprivate let locationManager:CLLocationManager = CLLocationManager()
+    
+    private var journey: Journey?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        //Setup location manager
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        //Configure map
+        self.configureMap()
+        
+    }
+    
+    private func configureMap() {
+        
+        //update location on main thread
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
+        }
+        
+            //Zoom to user location
+            if let userLocation = self.locationManager.location?.coordinate {
+                let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 500, longitudinalMeters: 500)
+                self.mapView.setRegion(viewRegion, animated: false)
+            }
+        
+        mapView.showsUserLocation = true
+    }
+    
+    
 
 }
+
 
